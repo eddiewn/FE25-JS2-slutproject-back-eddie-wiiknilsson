172142fda14dce7f28a2d4993d7828bbd76be601
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-04 20:11:58 +0100

    Kan nu lägga till en assignment via frontend, yippie!

--- src/AssignmentController/AddAssignments.ts
+++ src/AssignmentController/AddAssignments.ts
@@ -1,14 +1,25 @@
-import { readFile } from "fs/promises";
-import type { Assignments } from "../Types";
+// import { readFile } from "fs/promises";
+import fs from "fs";
+import type {Assignments} from "../Types";
 
-export const AddAssignments = async (newAssignments: Assignments) => {
-    try{
-        const assignments = await readFile("./src/database/assignments.json", 'utf-8')
-        const data: Assignments[] = JSON.parse(assignments);
+export const AddAssignments = async (newAssignments: Assignments[]) => {
+    try {
+        // const assignments = await readFile("./src/database/assignments.json", 'utf-8')
+        // const data: Assignments[] = JSON.parse(assignments);
 
-    }
-    catch{
-        const error = "Error receiving assignments"
+        fs.writeFile(
+            "./src/database/assignments.json",
+            JSON.stringify(newAssignments),
+            (error) => {
+                if (error) {
+                    console.log("Error writing file:", error);
+                    return;
+                }
+                console.log("File successfully rewritten!");
+            },
+        );
+    } catch {
+        const error = "Error adding assignment";
         return error;
     }
-}
+};
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import { GetAssignments } from "./AssignmentController/GetAssignments.ts";
 import { AddAssignments } from "./AssignmentController/AddAssignments.ts";
+// import { Assignments } from "./Types.ts";
 import { v4 as uuidv4 } from 'uuid';
 
 export const app = express();
@@ -18,20 +19,20 @@ app.post("/addAssignment", async(req, res) => {
 
     const assignments = await GetAssignments();
 
-    if(Array.isArray(assignments))
+    if(Array.isArray(assignments)){
 
-    assignments.push({
-        id: uuidv4(),
-        title: title,
-        description: description,
-        category: category,
-        status: "new",
-        assignedto: null,
-        timestamp: Date.now().toString()
-    })
-    console.log(assignments)
-
-
+        assignments.push({
+            id: uuidv4(),
+            title: title,
+            description: description,
+            category: category,
+            status: "new",
+            assignedto: null,
+            timestamp: Date.now().toString()
+        })
+        console.log(assignments)
+        AddAssignments(assignments)
+    }
 
     res.json("yup i got it")
 })
