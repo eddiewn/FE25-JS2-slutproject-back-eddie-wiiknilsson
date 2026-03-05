c28103fd25354e6c19d32681f456102c85666941
Eddie Wiik Nilsson <eddie.wiiknilsson@gmail.com>
2026-03-05 22:53:15 +0100

    Remove button now works

---
+++ src/AssignmentController/DeleteAssignments.ts
@@ -0,0 +1,22 @@
+// import { readFile } from "fs/promises";
+import fs from "fs";
+import type {Assignments} from "../Types";
+
+export const DeleteAssignments = async (newAssignments: Assignments[]) => {
+    try {
+        fs.writeFile(
+            "./src/database/assignments.json",
+            JSON.stringify(newAssignments),
+            (error) => {
+                if (error) {
+                    console.log("Error writing file:", error);
+                    return;
+                }
+                console.log("File successfully rewritten!");
+            },
+        );
+    } catch {
+        const error = "Error adding assignment";
+        return error;
+    }
+};
--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,34 @@
 import express from "express";
-import cors from "cors"
-import { GetAssignments } from "./AssignmentController/GetAssignments.ts";
-import { AddAssignments } from "./AssignmentController/AddAssignments.ts";
-import { GetMembers } from "./MemberController/GetMembers.ts";
-import { AddMember } from "./MemberController/AddMember.ts"
+import cors from "cors";
+import {GetAssignments} from "./AssignmentController/GetAssignments.ts";
+import {AddAssignments} from "./AssignmentController/AddAssignments.ts";
+import {GetMembers} from "./MemberController/GetMembers.ts";
+import {AddMember} from "./MemberController/AddMember.ts";
 // import AddMember
 // import { Assignments } from "./Types.ts";
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from "uuid";
+import {DeleteAssignments} from "./AssignmentController/DeleteAssignments.ts";
 
 export const app = express();
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get("/getAssignments", async(req, res) => {
+app.get("/getAssignments", async (req, res) => {
     const assignments = await GetAssignments();
     res.json(assignments);
-})
+});
 
 app.get("/getMembers", async (req, res) => {
     const members = await GetMembers();
     res.json(members);
-})
+});
 
-app.post("/addAssignment", async(req, res) => {
-    const { title, description, category } = await req.body;
+app.post("/addAssignment", async (req, res) => {
+    const {title, description, category} = await req.body;
 
     const assignments = await GetAssignments();
 
-    if(Array.isArray(assignments)){
-
+    if (Array.isArray(assignments)) {
         assignments.push({
             id: uuidv4(),
             title: title,
@@ -36,38 +36,52 @@ app.post("/addAssignment", async(req, res) => {
             category: category,
             status: "new",
             assignedto: null,
-            timestamp: Date.now().toString()
-        })
-        console.log(assignments)
-        AddAssignments(assignments)
+            timestamp: Date.now().toString(),
+        });
+        console.log(assignments);
+        AddAssignments(assignments);
     }
 
-    res.json("Successfully added assignment")
-})
+    res.json("Successfully added assignment");
+});
 
-app.post("/addMember", async(req, res) => {
-    const { name, category } = await req.body;
+app.post("/addMember", async (req, res) => {
+    const {name, category} = await req.body;
     const members = await GetMembers();
 
-    console.log("I am here")
+    console.log("I am here");
 
-    if(Array.isArray(members)){
+    if (Array.isArray(members)) {
         members.push({
             id: uuidv4(),
             name: name,
             category: category,
-        })
-    }else{
-        res.json("Wrong adding member :(")
+        });
+    } else {
+        res.json("Wrong adding member :(");
         return;
     }
 
-    AddMember(members)
-    res.json("Successfully added member")
-})
+    AddMember(members);
+    res.json("Successfully added member");
+});
 
-app.delete("/deleteAssignment", async(req, res) => {
-    const id = req.body.id
+app.delete("/deleteAssignment", async (req, res) => {
+    const id = req.body.id;
 
-    res.json(`Will attempt to remove with ID: ${id}`)
-})
+    const assignments = await GetAssignments();
+    const assignmentsIndex = assignments?.findIndex(
+        (assignment) => assignment.id === id,
+    );
+    console.log(assignmentsIndex);
+
+    if (assignmentsIndex) {
+        assignments?.splice(assignmentsIndex, 1);
+    }
+
+    if (assignments) {
+        DeleteAssignments(assignments);
+    }
+
+    res.json(`Will attempt to remove with ID: ${id}`);
+});
